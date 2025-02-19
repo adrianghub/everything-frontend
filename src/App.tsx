72106b43b02156371da89618d9b8c26254aa1466
Adrian Zinko <zinko.adrian00@gmail.com>
2025-02-19 17:46:47 +0100

    add version box component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { Outlet } from 'react-router-dom';
+import { AppVersionBox } from './components/AppVersionBox';
 import { BookmarksIndicator } from './components/BookmarksIndicator';
 
 const App = () => {
   return (
-    <div>
-      <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
-          <h1 className="text-xl font-bold">
-            🚀 Rick and Morty - Fan Service {__APP_VERSION__} - {import.meta.env.MODE}
-          </h1>
-          <BookmarksIndicator />
-          {/* <Suspense fallback={<div className="animate-pulse">Loading weather...</div>}>
+    <>
+      {import.meta.env.VITE_ENVIRONMENT !== 'PROD' ? <AppVersionBox /> : null}
+      <div>
+        <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
+            <h1 className="text-xl font-bold">🚀 Rick and Morty - Fan Service</h1>
+            <BookmarksIndicator />
+            {/* <Suspense fallback={<div className="animate-pulse">Loading weather...</div>}>
             <WeatherWidget />
           </Suspense> */}
+          </div>
         </div>
+        <Outlet />
       </div>
-      <Outlet />
-    </div>
+    </>
   );
 };
 
